e9d8b44d3d2197d9eb88766185f64700981b39e4
chamudiabeysinghe <cabeysinghe16@gmail.com>
2022-12-12 15:17:42 +0530

    Create ChampionshipManagerTest.java

---
+++ src/Test/com/company/ChampionshipManagerTest.java
@@ -0,0 +1,35 @@
+/*
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChampionshipManagerTest {
+
+    @org.junit.jupiter.api.Test
+    void addNewDriver() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteDriver() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void changeDriver() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void showStatistics() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void displayFormula1DriverTable() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addRaceCompleted() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveInformation() {
+    }
+}*/
