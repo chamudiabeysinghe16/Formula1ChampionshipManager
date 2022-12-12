55362b83619254e2bdb6e8fe658d1ac744f8fee1
chamudiabeysinghe <cabeysinghe16@gmail.com>
2022-12-12 15:17:51 +0530

    Create Formula1DriverTest.java

---
+++ src/Test/com/company/Formula1DriverTest.java
@@ -0,0 +1,85 @@
+/*
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Formula1DriverTest {
+
+    @Test
+    void getPoints() {
+    }
+
+    @Test
+    void setPoints() {
+    }
+
+    @Test
+    void getNoOfParticipatedRaces() {
+    }
+
+    @Test
+    void setNoOfParticipatedRaces() {
+    }
+
+    @Test
+    void getNumOfDrivers() {
+    }
+
+    @Test
+    void setNumOfDrivers() {
+    }
+
+    @Test
+    void getCarNumber() {
+    }
+
+    @Test
+    void setCarNumber() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getFirstPositionCount() {
+    }
+
+    @Test
+    void setFirstPositionCount() {
+    }
+
+    @Test
+    void getSecondPositionCount() {
+    }
+
+    @Test
+    void setSecondPositionCount() {
+    }
+
+    @Test
+    void getThirdPositionCount() {
+    }
+
+    @Test
+    void setThirdPositionCount() {
+    }
+
+    @Test
+    void getPosition() {
+    }
+
+    @Test
+    void setPosition() {
+    }
+
+    @Test
+    void getDate() {
+    }
+
+    @Test
+    void setDate() {
+    }
+}*/
