901aa199debb247d4220303d5da8a4ee3af9bbec
chamudiabeysinghe <cabeysinghe16@gmail.com>
2022-12-12 15:17:48 +0530

    Create Formula1ChampionshipManagerTest.java

---
+++ src/Test/com/company/Formula1ChampionshipManagerTest.java
@@ -0,0 +1,49 @@
+/*
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Formula1ChampionshipManagerTest {
+
+    @Test
+    void getDriversArray() {
+    }
+
+    @Test
+    void getRacesArray() {
+    }
+
+    @Test
+    void addNewDriver() {
+    }
+
+    @Test
+    void deleteDriver() {
+    }
+
+    @Test
+    void changeDriver() {
+    }
+
+    @Test
+    void showStatistics() {
+    }
+
+    @Test
+    void displayFormula1DriverTable() {
+    }
+
+    @Test
+    void addRaceCompleted() {
+    }
+
+    @Test
+    void saveInformation() {
+    }
+
+    @Test
+    void getInfo() {
+    }
+}*/
