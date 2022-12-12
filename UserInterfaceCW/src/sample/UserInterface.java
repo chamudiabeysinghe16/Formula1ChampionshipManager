f308ec4a28f658812169488248153ab22297c353
chamudiabeysinghe <cabeysinghe16@gmail.com>
2022-12-12 15:23:22 +0530

    Create UserInterface.java

---
+++ UserInterfaceCW/src/sample/UserInterface.java
@@ -0,0 +1,20 @@
+package sample;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ThreadLocalRandom;
+public class UserInterface {
+}
