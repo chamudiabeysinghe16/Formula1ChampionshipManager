09e07c0f87e6f63e8891cf329a03af1548b03386
chamudiabeysinghe <cabeysinghe16@gmail.com>
2022-12-12 15:17:46 +0530

    Create DriverTest.java

---
+++ src/Test/com/company/DriverTest.java
@@ -0,0 +1,53 @@
+/*
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DriverTest {
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void getNameOfDriver() {
+    }
+
+    @Test
+    void setNameOfDriver() {
+    }
+
+    @Test
+    void getLocation() {
+    }
+
+    @Test
+    void setLocation() {
+    }
+
+    @Test
+    void getTelNum() {
+    }
+
+    @Test
+    void setTelNum() {
+    }
+
+    @Test
+    void getInsuranceNum() {
+    }
+
+    @Test
+    void setInsuranceNum() {
+    }
+
+    @Test
+    void getCarManufacturer() {
+    }
+
+    @Test
+    void setCarManufacturer() {
+    }
+}*/
